2a3b59758663295080440bd13600080d7d10f275
dnemirich <dnemirich@gmail.com>
2025-02-09 00:13:59 +0100

    feat: finish preliminary layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
+import { SearchField } from './components/SearchField/SearchField.tsx';
+import { Cards } from './components/Cards/Cards.tsx';
 
 export function App() {
 
 
   return (
-    <>
-
-    </>
-  )
+    <main>
+      <div className={"container"}>
+        <div className={"wrapper"}>
+          <SearchField />
+          <Cards/>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 
