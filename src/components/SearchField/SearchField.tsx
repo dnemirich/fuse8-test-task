fe8ce4625060e90f39a282032eb5f825dcd042ad
dnemirich <dnemirich@gmail.com>
2025-02-09 00:13:08 +0100

    feat: add search field component

---
+++ src/components/SearchField/SearchField.tsx
@@ -0,0 +1,11 @@
+import s from './SearchField.module.scss';
+
+export const SearchField = () => {
+  return (
+    <div className={s.searchField}>
+      <input type={'text'} placeholder={'Search characters...'} autoFocus={true} className={s.input}/>
+      <span className={s.searchResultText}>Found characters:</span>
+    </div>
+
+  )
+}
