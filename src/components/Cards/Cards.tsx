7eab47ff35e0cc14f2e58162e6cb7602cfcae712
dnemirich <dnemirich@gmail.com>
2025-02-09 22:55:27 +0100

    feat: change cards container layout, add pagination

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,17 +1,39 @@
 import s from './Cards.module.scss';
 import { Card } from './Card/Card.tsx';
+import { useStore } from '../../store.ts';
+import { Pagination } from '@mui/material';
+import { type ChangeEvent, useEffect } from 'react';
 
 export const Cards = () => {
+  const { characters, charactersFound, currentPage, apiPage, setCurrentPage, setApiPage } = useStore();
+
+  const cardsPerPage = 8;
+  const apiPageSize = 20;
+
+  const paginationTotalPages = Math.ceil(charactersFound / cardsPerPage);
+  const startIndex = (currentPage - 1) * cardsPerPage;
+  const endIndex = startIndex + cardsPerPage;
+
+  const charactersToShow = characters.slice(startIndex, endIndex);
+
+  useEffect(() => {
+    const isNextApiPageNeeded = endIndex > characters.length && apiPage * apiPageSize < charactersFound;
+    if (isNextApiPageNeeded) {
+      setApiPage(apiPage + 1);
+    }
+  }, [currentPage])
+
+  const pageChangeHandler = (event: ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  }
+
   return (
-    <div className={s.cardsContainer}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+    <>
+      <div className={s.cardsContainer}>
+        { charactersToShow && charactersToShow.map(character => <Card key={character.id} character={character} />) }
+      </div>
+      { charactersFound > 8 && <Pagination count={paginationTotalPages} page={currentPage} onChange={pageChangeHandler} size={"large"} />}
+    </>
+
   );
 };
