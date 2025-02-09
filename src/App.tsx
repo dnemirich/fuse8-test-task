f2c08470912d4c179a07c5733b7d62fab64172d0
dnemirich <dnemirich@gmail.com>
2025-02-09 22:54:28 +0100

    feat: add loader bar to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import './App.css';
 import { SearchField } from './components/SearchField/SearchField.tsx';
 import { Cards } from './components/Cards/Cards.tsx';
+import { LinearProgress } from '@mui/material';
+import { useStore } from './store.ts';
 
 export function App() {
-
-
+  const { isLoading } = useStore();
   return (
-    <main>
-      <h1>Find a character from Rick and Morty series</h1>
-      <div className={'container'}>
-        <div className={'wrapper'}>
-          <SearchField />
-          <Cards />
+    <>
+      {isLoading && <LinearProgress style={{ color: '#656ec2' }} />}
+      <main>
+        <h1>Find a character from Rick and Morty series</h1>
+        <div className={'container'}>
+          <div className={'wrapper'}>
+            <SearchField />
+            <Cards />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
