ec3ba6edf5253a5f3af890cf9652e02ad72b68e2
dnemirich <dnemirich@gmail.com>
2025-02-09 22:54:57 +0100

    feat: change card layout

--- src/components/Cards/Card/Card.tsx
+++ src/components/Cards/Card/Card.tsx
@@ -1,12 +1,20 @@
 import s from './Card.module.scss';
+import type { CharacterType } from '../../../api/api.ts';
+import { formatDate } from '../../../utils/formatDate.ts';
 
-export const Card = () => {
+type PropsType = { character: CharacterType }
+
+export const Card = (props: PropsType) => {
+  const { character } = props;
+  const formattedDate = formatDate(character.created);
+
   return (
     <div className={s.card}>
-      <h2 className={s.cardTitle}>Stair Goblin - Mythological Creature</h2>
+      <h2 className={s.cardTitle}>{character.name}</h2>
       <div className={s.supportInfo}>
-        <span className={s.status}>Status: <span className={s.alive}>Alive</span></span>
-        <span className={s.date}>Created: 04.11.2017</span>
+        <span className={s.status}>Status: <span
+          className={character.status === 'Alive' ? s.alive : character.status === 'Dead' ? s.dead : ''}>{character.status}</span></span>
+        <span className={s.date}>Created: {formattedDate}</span>
       </div>
     </div>
   );
